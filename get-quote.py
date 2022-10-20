3b1d83cbcca8bee12a60fb3b820aaa90e8b54ee0
Samyak Sand <62803746+samyaksand@users.noreply.github.com>
2022-10-20 22:13:43 +0530

    Update with email sender

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,39 @@
-def main():
-  # print("Keep it logically awesome.")
+import requests, random
+from bs4 import BeautifulSoup
+#Created a random quote generator, and send it using email.
+# Web scraping implemented using BeautifulSoup
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+import smtplib
+def random_quote(url):
+    conn = requests.get(url)
+    soup = BeautifulSoup(conn.content,'html.parser')
+    quotes = soup.find_all('a', attrs={"title": "view quote"})
+    quotesList = []
+    for quote in quotes:
+        quotesList.append(quote.text)
 
-  #print(quotes)
+    num = random.randint(0,len(quotesList))
+    your_quote = 'Here is Your Quote:\n"'+quotesList[num]+'"'
+    return(print(your_quote))
 
-if __name__== "__main__":
-  main()
+def send_email(mail):
+    
+    sender = input("Enter Sender's Email ID:")
+    passwd = input("Enter Sender's Password:") 
+    
+    #using apps password is recommended. 
+    #or you'll have to enable less secure apps in your google account.
+    
+    reciever = input("Enter Reciever's Email ID:")
+    
+    server = smtplib.SMTP(host='smtp.gmail.com',port=587)
+    server.starttls()
+    server.ehlo()
+    server.login(sender,passwd)
+    server.sendmail(sender,reciever,mail)
+    print("Mail Sent Successfully!")
+
+
+msg = random_quote('https://www.brainyquote.com/topics/inspirational-quotes')
+mail = f'Subject: "Quote Of The Day"\n\n{msg}'
+send_email(mail)
